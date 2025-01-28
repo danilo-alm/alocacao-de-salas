cb35591fbeea22d1c58f6194391a0a6b465f6f6f
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-01-27 23:28:04 -0300

    initial commit

---
+++ src/main/java/com/ufal/alocacaodesalasbackend/AlocacaoDeSalasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufal.alocacaodesalasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocacaoDeSalasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlocacaoDeSalasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufal/alocacaodesalasbackend/AlocacaoDeSalasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufal.alocacaodesalasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocacaoDeSalasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
