80926aaedda1dfc00e7e1632deef756e92d1efad
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-08-07 21:23:07 -0300

    fix: id being passed for sala creation

--- src/sala/dto/create-sala.dto.ts
+++ src/sala/dto/create-sala.dto.ts
@@ -3,6 +3,7 @@ import { OmitType } from '@nestjs/swagger';
 import { SalaBaseDto } from './sala-base.dto';
 
 export class CreateSalaDto extends OmitType(SalaBaseDto, [
+  'id',
   'created_at',
   'updated_at',
 ] as const) {}
