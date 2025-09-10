d4b0cab3cc02a2d3d1870d850c75294fe031a7a9
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-09-10 16:18:03 -0300

    fix: correctly annotate dto for json parsing

--- src/solver/dto/solver-request.dto.ts
+++ src/solver/dto/solver-request.dto.ts
@@ -1,8 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested } from 'class-validator';
 import { SalaResponseDto } from 'src/sala/dto/sala-response.dto';
 
 import { PendingAlocacaoDto } from './pending-alocacao.dto';
 
 export class SolverRequestDto {
+  @ApiProperty({ type: () => [SalaResponseDto] })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SalaResponseDto)
   salas: SalaResponseDto[];
+
+  @ApiProperty({ type: () => [PendingAlocacaoDto] })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => PendingAlocacaoDto)
   pending: PendingAlocacaoDto[];
 }
